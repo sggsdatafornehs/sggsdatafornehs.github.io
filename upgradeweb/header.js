194c183583fdc91bb5b7e53245bc3ff5d7b21593
sggsdatafornehs <102708864+sggsdatafornehs@users.noreply.github.com>
2022-04-28 11:35:18 +0800

    Create header.js

---
+++ upgradeweb/header.js
@@ -0,0 +1,36 @@
+var a='<!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="index.html">科學班內容分享空間</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto py-4 py-lg-0"> 
+                        <h6><li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="thoughts.html" style="color:	#BEBEBE;">心得</a></li></h6>
+                        <h6><li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="about.html" style="color:	#BEBEBE">講解和教學</a></li></h6>
+                       <h6><li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="https://sggsdatafornehs.github.io/testing/login-system/" style="color:#BEBEBE">資源區</a></li></h6>
+                        
+                        <h6><li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.html" style="color:	#BEBEBE">我們的日常</a></li></h6>
+                     <h6><li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="https://sggsdatafornehs.github.io/upgradeweb/alertin.html" style="color:	#BEBEBE">重要公告</a></li></h6>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Page Header-->
+        <header class="masthead" style="background-image: url('https://sggsdatafornehs.github.io/upgradeweb/image/%E7%A7%91.jpg')">
+            <div class="container position-relative px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-md-10 col-lg-8 col-xl-7">
+                        <div class="site-heading">
+                            <h1>科學班內容分享空間</h1>
+                            <span class="subheading">這是由新竹市曙光女中附設國中部考上111學年度科園實中科學班學生架設的網站</span>
+<span class="subheading">歡迎大家都來參考喔!!!</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>'
+document.write(a);
